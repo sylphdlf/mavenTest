10c2098072d8afb4624d3260c3d14162726dff45
sylphdlf <sylphdlf@163.com>
2015-06-23 17:11:28 +0800

    2015-06-23

---
+++ account.email/src/main/java/com/dlf/myframe/base/App.java
@@ -0,0 +1,13 @@
+package com.dlf.myframe.base;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!11111" );
+    }
+}
